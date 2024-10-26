4361464f48d67217098013e86bf989817c3da229
Woraphon Choaywong <phoori12@gmail.com>
2024-10-26 17:07:50 +0200

    contact page

--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -1,43 +1,81 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className="grid gap-4 grid-cols-2 grid-rows-2">
+    <div className="grid gap-4 grid-cols-4 grid-rows-2 my-5">
      
-        <div>
+        <div className="flex flex-col items-center border-4 border-colors-grey_medium rounded max-w-[350px] my-3 justify-self-center">
         <Image 
             src="/fuse_webpage/dummy_prof.png"
             width={640}
             height={640}
             className="max-h-[320px] max-w-[320px] object-contain top-0 left-0"
-            alt="banner_left" />
+            alt="person1" />
+        <h1>Woraphon Choaywong</h1>
+        <h1>Team Leader</h1>
+        <Link href='mailto:w.choaywong@campus.tu-berlin.de'>w.choaywong@campus.tu-berlin.de</Link>
+        
         </div>
 
-        <div>
+        <div className="flex flex-col items-center border-4 border-colors-grey_medium rounded max-w-[350px] my-3 justify-self-center">
         <Image 
             src="/fuse_webpage/dummy_prof.png"
             width={640}
             height={640}
             className="max-h-[320px] max-w-[320px] object-contain top-0 left-0"
-            alt="banner_left" />
+            alt="person2" />
+        <h1>Denis Shcherba</h1>
+        <h1>Programming</h1>
+        <Link href='mailto:d.shcherba@campus.tu-berlin.de'>d.shcherba@campus.tu-berlin.de</Link>
         </div>
 
-        <div>
+        <div className="flex flex-col items-center border-4 border-colors-grey_medium rounded max-w-[350px] my-3 justify-self-center">
         <Image 
             src="/fuse_webpage/dummy_prof.png"
             width={640}
             height={640}
             className="max-h-[320px] max-w-[320px] object-contain top-0 left-0"
-            alt="banner_left" />
+            alt="person3" />
+        <h1>Eckart Cobo Briesewitz</h1>
+        <h1>Programming</h1>
+        <Link href='mailto:cobo-briesewitz@campus.tu-berlin.de'>cobo-briesewitz@campus.tu-berlin.de</Link>
         </div>
 
-        <div>
+        <div className="flex flex-col items-center border-4 border-colors-grey_medium rounded max-w-[350px] my-3 justify-self-center">
         <Image 
             src="/fuse_webpage/dummy_prof.png"
             width={640}
             height={640}
             className="max-h-[320px] max-w-[320px] object-contain top-0 left-0"
-            alt="banner_left" />
+            alt="person4" />
+        <h1>Patthicha Mahakijdechachai</h1>
+        <h1>Treasurer</h1>
+        <Link href='mailto:p.mahakijdechachai@campus.tu-berlin.de'>p.mahakijdechachai@campus.tu-berlin.de</Link>
+        </div>
+
+        <div className="flex flex-col items-center border-4 border-colors-grey_medium rounded max-w-[350px] my-3 justify-self-center col-start-2">
+        <Image 
+            src="/fuse_webpage/dummy_prof.png"
+            width={640}
+            height={640}
+            className="max-h-[320px] max-w-[320px] object-contain top-0 left-0"
+            alt="person3" />
+        <h1>Harry Bason</h1>
+        <h1>Mechanical Designs</h1>
+        <Link href='mailto:h.bason@campus.tu-berlin.de'>h.bason@campus.tu-berlin.de</Link>
+        </div>
+
+        <div className="flex flex-col items-center border-4 border-colors-grey_medium rounded max-w-[350px] my-3 justify-self-center">
+        <Image 
+            src="/fuse_webpage/dummy_prof.png"
+            width={640}
+            height={640}
+            className="max-h-[320px] max-w-[320px] object-contain top-0 left-0"
+            alt="person4" />
+        <h1>Phumin Lertpredanan</h1>
+        <h1>Electronics Designs</h1>
+        <Link href='mailto:p.lertpredanan@campus.tu-berlin.de'>p.lertpredanan@campus.tu-berlin.de</Link>
         </div>
 
     </div>
