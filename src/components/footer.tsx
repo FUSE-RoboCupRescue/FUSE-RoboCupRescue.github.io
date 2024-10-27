3d18cc48d69d05804d0319914623b76c68f4f172
Denis Shcherba <d.e.shcherba@gmail.com>
2024-10-27 15:10:52 +0100

    updated footer

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -5,11 +5,8 @@ export default function Footer() {
         <div>
             <div className="font-sans text-white bg-colors-grey_dark text-xl p-4">
                 <ul className="grid grid-flow-col gap-5 text-center items-center justify-start">
-                    <li>msg 1</li>
-                    <li>msg 2</li>
-                    <li>msg 3</li>
-                    <li>msg 4</li> 
-                    <li>mgs 5</li>
+                    <li>© 2024 FUSE RoboCupRescue Team</li>
+                    <li><a href="legal">Legal Notice</a></li>
                 </ul>
 
             </div>
