ef684b9f298f8708f94b04bf31623146028e4465
Denis Shcherba <d.e.shcherba@gmail.com>
2024-10-27 13:56:32 +0100

    outline test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -17,10 +17,39 @@ export default function Home() {
       </div>
 
 
-      <div className="text-base mt-5">
-        <p>Demo text</p>
+      <div className="text-base mt-5 space-y-8">
+        <h1 className="font-bold text-3xl text-colors-black text-center">Our Idea</h1>
+        <p className="text-center px-4 md:px-8">
+            We are <strong>FUSE</strong>, a dedicated team of students from the Technical University of Berlin, working together to push the boundaries of robotics technology.
+            Our focus is on developing advanced robotic solutions tailored for high-stakes, real-world scenarios. This year, our team is gearing up to participate in the 
+            <strong>RoboCup Rescue 2025 competition</strong>, which will be held in March 2025.
+        </p>
+
+        <h1 className="font-bold text-3xl text-colors-black text-center mt-8">Our Mission</h1>
+        <p className="text-center px-4 md:px-8">
+            Our primary goal is to design and build a robotic system capable of functioning effectively in emergency situations, such as natural disasters, search-and-rescue 
+            operations, and hostage scenarios. We envision a robot that can not only withstand challenging environments but also provide invaluable support to first responders, 
+            making rescue efforts faster, safer, and more effective.
+        </p>
+
+        <h1 className="font-bold text-3xl text-colors-black text-center mt-8">Our Vision</h1>
+        <p className="text-center px-4 md:px-8">
+            Our robot is intended to represent the future of emergency response technology. With advanced mobility features like tracks and flippers, alongside a versatile, 
+            mounted robotic arm, it is built to navigate through debris, rough terrain, and tight spaces. We're aiming to create a solution that can directly aid in life-saving 
+            efforts during critical rescue missions and be a dependable tool for those on the front lines in challenging, high-stakes scenarios.
+        </p>
+
+        <h1 className="font-bold text-3xl text-colors-black text-center mt-8">Searching for Sponsors</h1>
+        <p className="text-center px-4 md:px-">
+            We are actively looking for sponsors to support our project. Partnering with us offers several benefits for your business, including:
+        </p>
+        <ul className="list-disc ml-10 space-y-1.5 text-center px-4 md:px-8">
+            <li>Showcasing your company and its high-quality products through our innovative robot</li>
+            <li>Featuring your company logo on our team jerseys and the robot itself</li>
+            <li>Presenting the robot at one of your events to demonstrate the capabilities of your products</li>
+        </ul>
       </div>
-  
+
     </div>
 
   );
