1d1e6782be07f601b33ccfaad02e3d1b017b754a
Denis Shcherba <d.e.shcherba@gmail.com>
2025-02-04 20:43:03 +0100

    deployment test

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,8 +5,8 @@ import Footer from '../components/footer'
 import Image from "next/image";
 
 export const metadata: Metadata = {
-  title: "FUSE",
-  description: "RoboCup Rescue Team TU Berlin",
+  title: "thaigerlabs",
+  description: "ThaiGerLabs",
   icons: {
     icon: "./favicon.ico",
   },
@@ -20,20 +20,14 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className='bg-colors-white'>
-        <div className='flex justify-between bg-colors-grey_dark py-5' id="banner">
-          <Image 
-          src="./banner_left.png"
+        <div className='flex justify-between bg-[rgb(29,29,29)] py-5' id="banner">
+        <Image 
+          src="./thaigerlabslong.jpeg"
           width={500}
           height={200}
           className="max-h-[50px] max-w-[200px] sm:max-h-[100px] sm:max-w-[250px] md:max-w-[300px] lg:max-h-[200px] lg:max-w-[500px] object-contain top-0 left-0"
           alt="banner_left" />
 
-          <Image 
-          src="./banner_right.png"
-          width={350}
-          height={200}
-          className="max-h-[50px] max-w-[150px] sm:max-h-[100px] sm:max-w-[200px] md:max-w-[250px] lg:max-h-[200px] lg:max-w-[350px] object-contain top-0 left-0"
-          alt="banner_right" />
         </div>
         <Header></Header>
           <main>{children}</main>
--- src/app/team/page.tsx
+++ src/app/team/page.tsx
@@ -2,30 +2,36 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="flex flex-col my-5 xl:mx-[25rem] font-helvetica space-y-8">
+    <div className="flex flex-col my-5 xl:mx-[25rem] font-helvetica">
 
-      <h1 className="font-bold text-5xl md:text-7xl text-colors-black text-center">TEAM</h1>
+    {}
 
-      <div className="flex flex-col text-xs md:text-sm justify-center items-center font-normal">
-          <Image 
-            src="./team_lowres.jpg"
-            width={1080}
-            height={1080}
-            className="max-h-[270px] max-w-[270px] sm:max-h-[360px] sm:max-w-[360px] md:max-h-[540px] md:max-w-[540px] object-contain top-0 left-0 border-4 border-colors-grey_dark"
-            alt="team_photo" />
-          <h1 className="px-2 md:px-4">From left to right: Eckart, Denis, Woraphon, Phumin, Harry (Not included in the image: Patthicha)</h1>
+
+      <div className="text-sm md:text-base mt-5 space-y-8">
+      <h1 className="font-bold text-2xl md:text-5xl text-colors-black text-center">About us</h1>
       </div>
 
+
       <div className="px-4 md:px-8 mt-5 text-sm md:text-base">
-        <p>Our team is made up of dedicated students from TU Berlin, each bringing expertise in a variety of fields, including: </p>
-        <ul className="px-4 md:px-8 list-disc">
-          <li>Woraphon Choaywong, B.Sc. Computer Engineering</li>
-          <li>Denis Shcherba, M.Sc. Computer Engineering</li>
-          <li>Eckart Cobo Briesewitz, M.Sc. Computer Science</li>
-          <li>Patthicha Mahakijdechachai, B.Sc. Industrial Engineering</li>
-          <li>Harry Bason, B.Sc. Mechanical Engineering</li>
-          <li>Phumin Lertpredanan, B.Sc. Electrical Engineering</li>
-        </ul>       
+        <p>At ThaiGerLab, we are dedicated to providing the highest
+        quality medical cannabis to markets around the world.
+        Specializing in premium cannabis flowers, our company is
+        built on a foundation of excellence, ensuring that every
+        product we offer meets the strictest standards of purity and
+        potency.
+        Our mission is to support the health and well-being of our
+        customers by offering trusted, safe, and effective medical
+        cannabis solutions. We are fully committed to maintaining
+        rigorous quality control throughout every stage of
+        production, adhering to global standards like GoodManufacturing Practice (GMP) and Good Agricultural and
+        Collection Practices (GACP). By partnering exclusively
+        with licensed cultivators and manufacturers, we ensure that
+        our products are cultivated and processed with the utmost
+        care and precision.
+        Through our unwavering focus on quality, transparency,
+        and innovation, we strive to be a leading provider of
+        premium medical cannabis, helping improve the lives of
+        patients across the globe. </p>    
       </div>
     </div>
     
